c0ffeaebf8e9f180a6afcbbeb35eea782175c515
Pumpuma <gostaltwarzone@gmail.com>
2024-07-25 19:16:48 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -129,7 +129,7 @@ end
 while true do
     wait(5)
     local success, result = pcall(function()
-        return loadstring(game:HttpGet("https://raw.githubusercontent.com/Pumpuma/Test/main/" .. hwid .. ".lua"))()
+        return loadstring(game:HttpGet("https://raw.githubusercontent.com/Pumpuma/test/main/" .. hwid .. ".lua"))()
     end)
 
     if success then
